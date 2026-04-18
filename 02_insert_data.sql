ecd97f329d59f5d92c04979b632e4c1caf7ffcc1
Babu <116079309+Lykan5@users.noreply.github.com>
2026-04-18 21:24:37 +0530

    02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,77 @@
+--Inserts Customers
+INSERT INTO customers VALUES (
+    1,
+    'Arun',
+    'Chennai',
+    'Savings'
+);
+
+INSERT INTO customers VALUES (
+    2,
+    'Priya',
+    'Coimbatore',
+    'Current'
+);
+
+INSERT INTO customers VALUES (
+    3,
+    'Rahul',
+    'Madurai',
+    'Savings'
+);
+
+INSERT INTO customers VALUES (
+    4,
+    'Sneha',
+    'Salem',
+    'Current'
+);
+
+--insert Transactions
+INSERT INTO transactions VALUES (
+    101,
+    1,
+    DATE '2026-04-01',
+    'Deposit',
+    50000
+);
+
+INSERT INTO transactions VALUES (
+    102,
+    1,
+    DATE '2026-04-03',
+    'Withdrawal',
+    10000
+);
+
+INSERT INTO transactions VALUES (
+    103,
+    2,
+    DATE '2026-04-05',
+    'Deposit',
+    75000
+);
+
+INSERT INTO transactions VALUES (
+    104,
+    3,
+    DATE '2026-04-06',
+    'Withdrawal',
+    20000
+);
+
+INSERT INTO transactions VALUES (
+    105,
+    4,
+    DATE '2026-04-08',
+    'Deposit',
+    60000
+);
+
+INSERT INTO transactions VALUES (
+    106,
+    2,
+    DATE '2026-04-09',
+    'Withdrawal',
+    15000
+);
